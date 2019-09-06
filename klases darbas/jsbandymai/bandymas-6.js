55fdf2f3bea3c6ef9e343e75e2f1bee9ad29a79b
Rokas Ašmena <rokasasmena@gmail.com>
2019-09-06 15:03:11 +0300

    įkeliama klasės darbas

---
+++ klases darbas/jsbandymai/bandymas-6.js
@@ -0,0 +1,3 @@
+<button type="button" onclick="zyx.src=target.scene7.com/is/image/Target/GUEST_c87bc57e-b3f8-4461-bf56-bbc76657ade5?fmt=webp&wid=1400&qlt=80">Įjungti</button>
+
+let zyx = document.getElementsByClassName(zyx.src=target.scene7.com/is/image/Target/GUEST_c87bc57e-b3f8-4461-bf56-bbc76657ade5?fmt=webp&wid=1400&qlt=80">Įjungti</button>)
